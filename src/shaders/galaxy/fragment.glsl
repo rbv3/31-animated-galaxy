4a1ccf1ad2c4d5d7d1c6165ba1c1309de8e1a40d
Rodrigo Valenca <rvalenca@amazon.com>
2023-10-13 19:56:04 -0300

    animated galaxy

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -10,6 +10,8 @@ void main() {
     float strength = distance(gl_PointCoord, vec2(0.5));
     strength = 1.0 - strength;
     strength = pow(strength, 10.0);
+
     vec3 color = mix(vec3(0.0), vColor, strength);
-    gl_FragColor = vec4(vec3(color), 1.0);
+    
+    gl_FragColor = vec4(color, 1.0);
 }
--- src/shaders/galaxy/vertex.glsl
+++ src/shaders/galaxy/vertex.glsl
@@ -1,12 +1,26 @@
 uniform float uSize;
+uniform float uTime;
 
 attribute float aScale;
+attribute vec3 aRandomness;
 
 varying vec3 vColor;
 
 void main() {
     // Position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // Spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceFromCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceFromCenter) * uTime * 0.2;
+    angle += angleOffset;
+    modelPosition.x = cos(angle) * distanceFromCenter;
+    modelPosition.z = sin(angle) * distanceFromCenter;
+
+    // Randomness
+    modelPosition.xyz += aRandomness;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
