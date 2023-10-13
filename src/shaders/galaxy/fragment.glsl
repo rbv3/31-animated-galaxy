4a290dd9019059eb3ab4a24ca2f16fefc5471cf5
Rodrigo Valenca <rvalenca@amazon.com>
2023-10-13 19:33:43 -0300

    light pattern on particles

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,12 @@
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    // Diffuse point
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength = 2.0 * strength;
+    // strength = 1.0 - strength;
+    
+    // Light point
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = 1.0 - strength;
+    strength = pow(strength, 10.0);
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
